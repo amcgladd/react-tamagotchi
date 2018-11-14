830dd86576fdd2b79bd8581d4ffafe67e6ece585
AJ Mcgladdery and Kendra Kelly <aj+kendra@>
2018-11-14 14:40:22 -0800

    get the setInterval working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,8 +28,29 @@ class App extends React.Component {
     let newFulfilledNeed = newTamagotchi[need] + 10;
     newTamagotchi[need] = newFulfilledNeed;
     newTamagotchiArray[0] = newTamagotchi;
-    // console.log(newTamagotchiArray);
+    this.setState({
+      tamagotchi: newTamagotchiArray
+    }, () => {
+      console.log(this.state.tamagotchi[0]);
+    });
+  }
 
+  componentDidMount() {
+    this.increaseTamaNeed = setInterval(() =>
+      this.getNeedierOverTime(),
+    500);
+  }
+
+  getNeedierOverTime() {
+    let newTamagotchiArray = this.state.tamagotchi.slice();
+    let newTamagotchi = Object.assign({}, newTamagotchiArray[0]);
+    let needFood = newTamagotchi.feedNeed - 20;
+    let needSleep = newTamagotchi.sleepNeed - 10;
+    let needPlay = newTamagotchi.playNeed - 15;
+    newTamagotchi.feedNeed = needFood;
+    newTamagotchi.sleepNeed = needSleep;
+    newTamagotchi.playNeed = needPlay;
+    newTamagotchiArray[0] = newTamagotchi;
     this.setState({
       tamagotchi: newTamagotchiArray
     }, () => {
